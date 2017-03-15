3a8de398c44268aaa89ebfd15474903ffa734741
Andrey Kabylin <andrey@kabylin.ru>
2017-03-15 15:13:57 +0700

    History dataSetChanged on finish Dashboard

--- app/src/main/java/co/humaniq/views/CameraActivity.java
+++ app/src/main/java/co/humaniq/views/CameraActivity.java
@@ -18,66 +18,9 @@ import java.util.Date;
 
 
 public class CameraActivity extends ToolbarActivity {
-    private Camera camera;
-    private CameraPreview cameraPreview;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_photo);
-        camera = getCameraInstance();
-        cameraPreview = new CameraPreview(this, camera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(cameraPreview);
-
-        Button captureButton = (Button) findViewById(R.id.button_capture);
-        captureButton.setOnClickListener(v -> {
-            camera.takePicture(null, null, picture);
-            finish();
-        });
-    }
-
-    private Camera getCameraInstance() {
-        Camera camera = null;
-        try {
-            camera = Camera.open();
-        } catch (Exception e) {
-            // cannot get camera or does not exist
-        }
-        return camera;
-    }
-
-    Camera.PictureCallback picture = (data, camera1) -> {
-        File pictureFile = getOutputMediaFile();
-        if (pictureFile == null) {
-            return;
-        }
-        try {
-            FileOutputStream fos = new FileOutputStream(pictureFile);
-            fos.write(data);
-            fos.close();
-        } catch (FileNotFoundException e) {
-        } catch (IOException e) {
-        }
-    };
-
-    private static File getOutputMediaFile() {
-        File mediaStorageDir = new File(
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                "MyCameraApp");
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
-                Log.d("MyCameraApp", "failed to create directory");
-                return null;
-            }
-        }
-        // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
-                .format(new Date());
-        File mediaFile;
-        mediaFile = new File(mediaStorageDir.getPath() + File.separator
-                + "IMG_" + timeStamp + ".jpg");
-
-        return mediaFile;
     }
 }
